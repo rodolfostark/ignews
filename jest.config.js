43fc29342843607b02a0d4e775d6cbf5ba71ca45
Jonatas Rodolfo Pereira dos Santos <rodolfojonatas@outlook.com>
2023-06-07 11:19:21 -0300

    test: setting up the render tests for activelink

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    tesIgnorePatterns: ["/node_modules/", "/.next/"],
+    testPathIgnorePatterns: ["/node_modules/", "/.next/"],
     setupFilesAfterEnv: [
         "<rootDir>/src/tests/setupTests.ts"
     ],
--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,5 +1,30 @@
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react'
+import { ActiveLink } from '.'
+
+jest.mock('next/router', () => {
+    return {
+        useRouter() {
+            return {
+                asPath: '/'
+            }
+        }
+    }
+})
 
 test('active link renders correctly', () => {
+    const { getByText } = render(
+        <ActiveLink href="/" activeClassName="active">
+            <a>Home</a>
+        </ActiveLink>
+    )
+    expect(getByText('Home')).toBeInTheDocument()
+})
 
+test('active link is recieving active class', () => {
+    const { getByText } = render(
+        <ActiveLink href="/" activeClassName="active">
+            <a>Home</a>
+        </ActiveLink>
+    )
+    expect(getByText('Home')).toHaveClass('active')
 })
