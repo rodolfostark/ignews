11c174233ede11fbe1bc0aa6c2f360575d19aec7
Jonatas Rodolfo Pereira dos Santos <rodolfojonatas@outlook.com>
2023-06-11 23:18:52 -0300

    test: finished the home suite

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from '@testing-library/react'
 import { useSession } from 'next-auth/react'
-import Home from '../../pages'
 
+import { stripe } from '../../services/stripe'
+import Home, { getStaticProps } from '../../pages'
 
+jest.mock('next/router')
 jest.mock('next-auth/react')
+jest.mock('../../services/stripe')
 
 describe('Home page', () => {
     it('renders correctly', () => {
@@ -13,5 +16,25 @@ describe('Home page', () => {
             <Home product={{priceId: 'fake-price-id', amount: "$9.90"}} />
         )
         expect(screen.getByText("for $9.90 month")).toBeInTheDocument()
+    })
+
+    it('loads initial data', async () => {
+        const retrieveStripePricesMocked = jest.mocked(stripe.prices.retrieve)
+        retrieveStripePricesMocked.mockResolvedValueOnce({
+            id: 'fake-price-id',
+            unit_amount: 1000,
+        } as any)
+
+        const response = await getStaticProps({})
+        expect(response).toEqual(
+            expect.objectContaining({
+                props: {
+                    product: {
+                        priceId: 'fake-price-id',
+                        amount: '$10.00'
+                    }
+                }
+            })
+        )
     })
 })
