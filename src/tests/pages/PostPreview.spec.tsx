db0a051afbb1ba9f093b98ea84e5e1a81cc47c77
Jonatas Rodolfo Pereira dos Santos <rodolfojonatas@outlook.com>
2023-06-25 18:50:21 -0300

    test: finish post preview suite

--- src/tests/pages/PostPreview.spec.tsx
+++ src/tests/pages/PostPreview.spec.tsx
@@ -60,46 +60,40 @@ describe('Post preview page', () => {
         expect(pushMock).toHaveBeenCalledWith('/posts/my-new-post')
     })
 
-    // it('loads initial data', async () => {
-    //     const getSessionMocked = jest.mocked(getSession)
-    //     const getPrismicClientMocked = jest.mocked(getPrismicClient)
+    it('loads initial data', async () => {
+        const getPrismicClientMocked = jest.mocked(getPrismicClient)
+        getPrismicClientMocked.mockReturnValueOnce({
+            getByUID: jest.fn().mockResolvedValueOnce({
+                uid: 'my-new-post',
+                data: {
+                    title: [
+                        { type: 'heading', text: 'My New Post' }
+                    ],
+                    content: [
+                        { type: 'paragraph', text: 'Post Excerpt' }
+                    ]
+                },
+                last_publication_date: '04-01-2021'
+            })
+        } as any)
+        
+        const response = await getStaticProps({
+            params: {
+                slug: 'my-new-post'
+            }
+        } as any)
 
-    //     getPrismicClientMocked.mockReturnValueOnce({
-    //         getByUID: jest.fn().mockResolvedValueOnce({
-    //             uid: 'my-new-post',
-    //             data: {
-    //                 title: [
-    //                     { type: 'heading', text: 'My New Post' }
-    //                 ],
-    //                 content: [
-    //                     { type: 'paragraph', text: 'Post Excerpt' }
-    //                 ]
-    //             },
-    //             last_publication_date: '04-01-2021'
-    //         })
-    //     } as any)
-
-    //     getSessionMocked.mockResolvedValueOnce({
-    //         activeSubscription: 'fake-active-subscription',
-    //     } as any)
-
-    //     const response = await getServerSideProps({
-    //         params: {
-    //             slug: 'my-new-post'
-    //         }
-    //     } as any)
-
-    //     expect(response).toEqual(
-    //         expect.objectContaining({
-    //             props: {
-    //                 post: {
-    //                     slug: 'my-new-post',
-    //                     title: 'My New Post',
-    //                     content: '<p>Post Excerpt</p>',
-    //                     updatedAt: '01 de abril de 2021'
-    //                 }
-    //             }
-    //         })
-    //     )
-    // })
+        expect(response).toEqual(
+            expect.objectContaining({
+                props: {
+                    post: {
+                        slug: 'my-new-post',
+                        title: 'My New Post',
+                        content: '<p>Post Excerpt</p>',
+                        updatedAt: '01 de abril de 2021'
+                    }
+                }
+            })
+        )
+    })
 })
